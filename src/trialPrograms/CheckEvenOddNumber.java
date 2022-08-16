d4f8f5839693e6e17150819323226fa173f607d6
Ganeshital <Ganeshital@ShitalGanesh>
2022-08-16 10:20:56 +0530

    commited

---
+++ src/trialPrograms/CheckEvenOddNumber.java
@@ -0,0 +1,15 @@
+package trialPrograms;
+
+public class CheckEvenOddNumber {
+
+	public static void main(String[] args) {
+		int a=1234565;
+		if(a%2==0) {
+			System.out.println("the number is even");
+		}
+		else {
+			System.out.println("the number is odd");
+		}
+		
+	}
+}
