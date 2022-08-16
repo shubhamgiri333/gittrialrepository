8190f2f4f2bd94619be408dc87b97c9a122b8328
Ganeshital <Ganeshital@ShitalGanesh>
2022-08-16 10:46:42 +0530

    bytesss

---
+++ src/trialPrograms/Byte.java
@@ -0,0 +1,13 @@
+package trialPrograms;
+
+public class Byte {
+	public static void main(String[] args) {
+		
+		byte a=45; 
+				System.out.println(a);
+				
+				 byte b = -25;
+				System.out.println(b);
+	}
+
+}
